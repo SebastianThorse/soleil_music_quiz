a5487114d8d442488f678875caa488dcf34a725f
SebastianThorse <sebbethorsen@gmail.com>
2026-02-05 17:58:37 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import db from '@astrojs/db';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [db(), react()],
+});
